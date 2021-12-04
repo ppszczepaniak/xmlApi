80cf1c36fae9f763283781504b44ffb121b687b2
ppszczepaniak <p.p.szczepaniak@gmail.com>
2021-12-04 11:49:53 +0100

    initial commit

---
+++ src/test/java/com/example/xmlapi/XmlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xmlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
